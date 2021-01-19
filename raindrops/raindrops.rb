ad40e38b1491ab8dd6112b9043c8073999bd10fd
shiva <rootofshiva@gmail.com>
2021-01-19 13:25:55 +0530

    raindrops

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -5,4 +5,13 @@ Write your code for the 'Raindrops' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/raindrops` directory.
 =end
-
+class Raindrops
+    def self.convert n 
+        song = ""
+        song += "Pling" if n % 3 == 0
+        song += "Plang" if n % 5 == 0
+        song += "Plong" if n % 7 == 0
+        return n.to_s if song.empty?
+        return song if !song.empty?
+    end
+end
